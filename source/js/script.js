3dcd9c583d47390003ac7f117aa03d6bdd35ba2c
Alena Zueva <aleo.zueva@gmail.com>
2020-07-21 12:01:55 +0700

    first commit

---
+++ source/js/script.js
@@ -0,0 +1,30 @@
+'use strict';
+(function () {
+  var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    breakpoints: {
+      576: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+      992: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
+})();
